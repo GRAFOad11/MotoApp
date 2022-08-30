f59e3c2cf1400184b3752fd86c109a7f0d9b555c
Adrian <matusiaka11@gmail.com>
2022-08-30 22:46:02 +0200

    module 1

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -1,11 +1,17 @@
 ﻿using MotoApp;
 
-var stack = new BasicStack();
-stack.Push(items: 4.5);
-stack.Push(items: 43);
-stack.Push(items: 333.6);
+var stack = new Stack<double>();
+
+stack.Push(item: 4.5);
+stack.Push(item: 43);
+stack.Push(item: 333.6);
 
 double sum = 0.0;
+var stackString = new BasicStack<string>();
+
+stackString.Push(item: "Adrian");
+stackString.Push(item: "Ania");
+stackString.Push(item: "Kevin");
 
 while (stack.Count > 0)
 {
@@ -15,3 +21,11 @@ while (stack.Count > 0)
 }
 
 Console.WriteLine($"Sum: {sum}");
+
+while (stackString.Count > 0)
+{
+    string item = stackString.Pop();
+    Console.WriteLine($"Item: {item}");
+    
+}
+
