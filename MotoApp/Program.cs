9eb311c3a79305c56a8d6aa6f241b366cd601106
Adrian <matusiaka11@gmail.com>
2022-08-30 21:37:20 +0200

    Update Program.cs

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -1,3 +1,17 @@
 ﻿using MotoApp;
 
 var stack = new BasicStack();
+stack.Push(items: 4.5);
+stack.Push(items: 43);
+stack.Push(items: 333.6);
+
+double sum = 0.0;
+
+while (stack.Count > 0)
+{
+    double item = stack.Pop();
+    Console.WriteLine($"Item: {item}");
+    sum += item;
+}
+
+Console.WriteLine($"Sum: {sum}");
